2fe802f3775dadc791e1c714adef680628e40414
Alex Mortensen <alextmortensen@gmail.com>
2019-09-20 23:21:07 -0600

    Adds get bro notes

--- src/routes/get-bro-notes.route.js
+++ src/routes/get-bro-notes.route.js
@@ -1,9 +1,33 @@
 const { BroNotes } = require('../models');
+const geolib = require('geolib');
 
 module.exports = async (req, res, next) => {
   try {
-    // in a 25 mile radius
-    const broNotes = await BroNotes.find().populate('bro-votes');
+    if (!req.query.latitude || !req.query.longitude) {
+      return res
+        .status(400)
+        .json({ message: 'Bro! We need your coordinates.' });
+    }
+
+    const metersIn25Miles = 40233.6;
+
+    const bounds = geolib.getBoundsOfDistance(
+      {
+        latitude: req.query.latitude,
+        longitude: req.query.longitude
+      },
+      metersIn25Miles / 2
+    );
+
+    const maxLatitude = bounds[1].latitude;
+    const minLatitude = bounds[0].latitude;
+    const maxLongitude = bounds[1].longitude;
+    const minLongitude = bounds[0].longitude;
+
+    const broNotes = await BroNotes.find({
+      latitude: { $gt: minLatitude, $lt: maxLatitude },
+      longitude: { $gt: minLongitude, $lt: maxLongitude }
+    });
 
     res.status(200).json(broNotes);
   } catch (e) {
