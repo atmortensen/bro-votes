267c1d28ad276da1fe661e580ecbf8eb14227982
Alex Mortensen <alextmortensen@gmail.com>
2019-09-23 16:43:10 -0600

    Naughty Naughty

--- src/routes/sign-up.route.js
+++ src/routes/sign-up.route.js
@@ -1,11 +1,24 @@
 const { Bro } = require('../models');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const reCAPTCHA = require('recaptcha2');
+
+const recaptcha = new reCAPTCHA({
+  siteKey: process.env.RECAPTCHA,
+  secretKey: process.env.RECAPTCHA_SECRET
+});
 
 module.exports = async (req, res, next) => {
   try {
     if (await Bro.findOne({ handle: req.body.handle.toLowerCase().trim() })) {
       return res.status(400).json({ message: 'Handle not available.' });
+    }
+
+    if (
+      !req.body.recaptcha ||
+      !(await recaptcha.validate(req.body.recaptcha))
+    ) {
+      return res.status(400).json({ message: "You're a naughty bro." });
     }
 
     const bro = await Bro.create({
